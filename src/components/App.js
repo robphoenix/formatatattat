042c7d2e7ee1817d6af621dd684f54742c75dfdc
Rob Phoenix <rob@robphoenix.com>
2018-05-17 11:00:59 +0100

    Format JSON

--- src/components/App.js
+++ src/components/App.js
@@ -7,14 +7,33 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      value: "Paste your JSON here."
+      inputValue: "Paste your JSON here.",
+      outputValue: "",
+      hasError: false,
+      errorValue: ""
     }
 
     this.handleChange = this.handleChange.bind(this)
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value })
+    this.setState({ inputValue: event.target.value })
+    let parsed = ""
+    try {
+      parsed = JSON.parse(event.target.value)
+    } catch (e) {
+      this.setState({
+        hasError: true,
+        errorValue: e
+      })
+      return
+    }
+    const formatted = JSON.stringify(parsed, null, 2)
+    this.setState({
+      outputValue: formatted,
+      hasError: false,
+      errorValue: ""
+    })
   }
 
   render() {
@@ -23,8 +42,15 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Formatter-tat-tat</h1>
         </header>
-        <InputArea value={this.state.value} handleChange={this.handleChange} />
-        <OutputArea value={this.state.value} />
+        <InputArea
+          value={this.state.inputValue}
+          handleChange={this.handleChange}
+        />
+        <OutputArea
+          hasError={this.state.hasError}
+          value={this.state.outputValue}
+          error={this.state.errorValue}
+        />
       </div>
     )
   }
--- src/components/OutputArea.js
+++ src/components/OutputArea.js
@@ -2,9 +2,16 @@ import React, { Component } from "react"
 
 export default class OutputArea extends Component {
   render() {
+    if (this.props.hasError) {
+      return (
+        <div className="output-area">
+          <p>Error: {this.props.error.toString()}</p>
+        </div>
+      )
+    }
     return (
       <div className="output-area">
-        <p>{this.props.value}</p>
+        <pre>{this.props.value}</pre>
       </div>
     )
   }
